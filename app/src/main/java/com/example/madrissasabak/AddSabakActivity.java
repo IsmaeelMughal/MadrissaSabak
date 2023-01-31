df7c2b51b4746b819f6c9edfa9f19725007477a6
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-01-31 22:51:39 +0500

    Add Sabak Page Added

---
+++ app/src/main/java/com/example/madrissasabak/AddSabakActivity.java
@@ -0,0 +1,14 @@
+package com.example.madrissasabak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSabakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sabak);
+    }
+}
